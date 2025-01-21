c6f62c9a4e435eeab778d24b1aa566bd3302afef
Oleksandr <alexundercover191@gmail.com>
2025-01-21 11:29:24 +0200

    Add customer stats management to CustomerStatsController
    Updated CustomerStatsController with new functionality:
    - Initialized air supply and air decrease rate variables.
    - Added visiting time and max visiting time variables.
    - Added CustomerBehaviour reference and initialized in Awake.
    - Set visiting time to a random value in Start.
    - Updated customer stats in Update method.
    - Managed air supply and visiting time in UpdateCustomerStats.

--- Assets/Scripts/Components/CustomerStatsController.cs
+++ Assets/Scripts/Components/CustomerStatsController.cs
@@ -2,5 +2,45 @@ using UnityEngine;
 
 public class CustomerStatsController : MonoBehaviour
 {
-    
+    private float air = 100f;
+    private float airDecreaseRate = 0.1f;
+    private float visitingTime = 100f;
+    private float maxVisitingTime = 200f;
+
+    private CustomerBehaviour customerBehaviour;
+
+    private void Awake()
+    {
+        customerBehaviour = GetComponent<CustomerBehaviour>();
+    }
+
+    private void Start()
+    {
+        visitingTime = Random.Range(visitingTime, maxVisitingTime);
+    }
+
+    private void Update()
+    {
+        UpdateCustomerStats();
+    }
+
+    private void UpdateCustomerStats()
+    {
+        if (customerBehaviour.IsInWater)
+        {
+            air -= airDecreaseRate * Time.deltaTime;
+            if (air <= 0)
+            {
+                // Customer is dead
+            }
+        }
+        else
+        {
+            visitingTime -= Time.deltaTime;
+            if (visitingTime <= 0)
+            {
+                // Cutomer is leaving
+            }
+        }
+    }
 }
