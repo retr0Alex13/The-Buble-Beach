e1da1bb59bb26659c9b9a4d7f6bfcbc01ff6940a
Oleksandr <alexundercover191@gmail.com>
2025-01-22 15:49:10 +0200

    Add GameInstaller with Zenject bindings and serialized fields

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    [SerializeField] private Transform jumpPoint;
+    [SerializeField] private Transform startPoint;
+
+    [SerializeField] private GameObject customerPrefab;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<Transform>().WithId("JumpPoint").FromInstance(jumpPoint);
+        Container.Bind<Transform>().WithId("StartPoint").FromInstance(startPoint);
+
+        Container.Bind<MoveCustomer>().FromComponentInNewPrefab(customerPrefab).AsTransient();
+
+        Container.Bind<GameObject>().WithId("CustomerPrefab").FromInstance(customerPrefab);
+    }
+}
