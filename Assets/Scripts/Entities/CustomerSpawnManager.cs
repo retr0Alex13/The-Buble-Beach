448f1f7acd3b4fcf65cc893efc0545c3f613c373
Oleksandr <alexundercover191@gmail.com>
2025-01-24 00:15:18 +0200

    Add customer spawning logic and bind DrownLine transform
    Updated CustomerSpawnManager.cs to include new functionality for spawning customers at random intervals, added necessary fields, and replaced direct instantiation with a coroutine. Introduced a method to handle customer departures. Updated GameInstaller.cs to bind a new Transform instance with the ID "DrownLine" for dependency injection.

--- Assets/Scripts/Entities/CustomerSpawnManager.cs
+++ Assets/Scripts/Entities/CustomerSpawnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Zenject;
 
@@ -6,11 +7,36 @@ public class CustomerSpawnManager : MonoBehaviour
     [SerializeField] private Transform spawnPoint;
     [Inject(Id = "CustomerPrefab")] private GameObject customerPrefab;
 
+    [SerializeField] private float spawnInterval = 5f;
+    [SerializeField] private float maxSpawnInterval = 10f;
+    [SerializeField] private int maxCustomers = 5;
+
+    private int currentCustomers;
+
     [Inject] private DiContainer container;
 
     private void Start()
     {
-        GameObject customerInstance = container.InstantiatePrefab(customerPrefab, spawnPoint.position, Quaternion.identity, null);
-        MoveCustomer moveCustomer = customerInstance.GetComponent<MoveCustomer>();
+        StartCoroutine(SpawnVisitors());
+    }
+
+    private IEnumerator SpawnVisitors()
+    {
+        while (true)
+        {
+            if (currentCustomers < maxCustomers)
+            {
+                GameObject customerInstance = container.InstantiatePrefab(customerPrefab, spawnPoint.position, Quaternion.identity, null);
+                currentCustomers++;
+            }
+            float randomSpawnInterval = Random.Range(spawnInterval, maxSpawnInterval);
+            yield return new WaitForSeconds(randomSpawnInterval);
+        }
+    }
+
+    public void CustomerLeft(GameObject customer)
+    {
+        currentCustomers--;
+        Destroy(customer);
     }
 }
--- Assets/Scripts/GameInstaller.cs
+++ Assets/Scripts/GameInstaller.cs
@@ -5,6 +5,7 @@ public class GameInstaller : MonoInstaller
 {
     [SerializeField] private Transform jumpPoint;
     [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform drownLine;
     [SerializeField] private GameObject customerPrefab;
 
     [SerializeField] private Transform emergePoint;
@@ -27,6 +28,7 @@ public class GameInstaller : MonoInstaller
     {
         Container.Bind<Transform>().WithId("JumpPoint").FromInstance(jumpPoint);
         Container.Bind<Transform>().WithId("StartPoint").FromInstance(startPoint);
+        Container.Bind<Transform>().WithId("DrownLine").FromInstance(drownLine);
 
         Container.Bind<MoveCustomer>().FromComponentInNewPrefab(customerPrefab).AsTransient();
         Container.Bind<GameObject>().WithId("CustomerPrefab").FromInstance(customerPrefab);
